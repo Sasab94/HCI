ec27bbde0d8cb42698ad2c3c9edca5ed77a76cf0
Sasab94 <cipi_94@hotmail.com>
2022-04-22 16:24:17 +0200

    Dodane stavke menija

---
+++ HCI/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HCI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Novi_Dogadjaj_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Novi_Tip_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Nova_Etiketa_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Dogadjaj_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Tip_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Etiketa_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
